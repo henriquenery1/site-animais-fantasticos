bf05e47120b2f563161f0fd8d74e4d45d8c1bf03
Henrique Nery <88451907+henriquenery1@users.noreply.github.com>
2021-09-06 00:42:06 -0300

    Add files via upload

---
+++ aula/script-aula2.js
@@ -0,0 +1,18 @@
+const imgs = document.querySelectorAll('img');
+
+let i = 0;
+imgs.forEach(function(item, index, array) {
+// console.log(item, index, array)
+});
+
+const titulos = document.getElementsByClassName('titulo');
+const titulosArray = Array.from(titulos);
+titulosArray.forEach(function(item){
+//console.log(item);
+});
+
+const img = document.querySelectorAll('img');
+img.forEach((item) => {
+ console.log(item);
+});
+
